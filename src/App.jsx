0d8c3ec0e858a87be72255a005dac9870d056804
ragul24cogniwide <ragulvasanth.muthukumar@cogniwide.com>
2025-08-19 18:02:04 +0530

    git push conflict changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-<<<<<<< HEAD
 import CogniXcelerate from "./Pages/CogniXcelerate";
 import './App.css'
-=======
 import Header from './Components/Header';
-import './App.css';
->>>>>>> aa0ac2cdb10781cd9094f7f7f987d51183da5ede
+
 
 function App() {
   return (
