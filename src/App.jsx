e9df15fb09d1b70ea232dc48e68befb804eb4e4f
ragul24cogniwide <ragulvasanth.muthukumar@cogniwide.com>
2025-08-19 16:58:46 +0530

    pushed the routing the changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
 import './App.css'
 
 function App() {
-
   return (
-     <BrowserRouter>
-     <Routes>
-      <Route path="/" element={<Home />} />
-     </Routes>
-     </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
+
 
 export default App
