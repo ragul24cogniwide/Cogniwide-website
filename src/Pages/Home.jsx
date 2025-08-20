6b7769a9b8546de5717a07bd6b61ca73a93c5815
deepakselvamcogniwide <deepak.selvam@cogniwide.com>
2025-08-19 21:35:03 -0700

    commit changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            <h1 className='text-3xl font-bold underline text-red-500'>Home</h1>
+            <h1 className='text-3xl font-bold underline text-red-500'>Cogniwide</h1>
         </div>
     )
 }
